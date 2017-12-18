7e961a35454c96a26a752b7342eb398685780042
Karl Hiner <karl.hiner@gmail.com>
2017-12-17 18:33:02 -0800

    Set DEBUG=False for Heroku

--- python_crash_course/learning_log/learning_log/settings.py
+++ python_crash_course/learning_log/learning_log/settings.py
@@ -141,7 +141,9 @@ if cwd == '/app' or cwd[:4] == '/tmp':
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['learning-log-pcc.herokuapp.com']
+
+    DEBUG = False
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
