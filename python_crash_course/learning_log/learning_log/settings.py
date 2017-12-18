4baaf001b698df0ddfa90afb588a53aefa8d6d86
Karl Hiner <karl.hiner@gmail.com>
2017-12-17 17:04:38 -0800

    fix typo in settings

--- python_crash_course/learning_log/learning_log/settings.py
+++ python_crash_course/learning_log/learning_log/settings.py
@@ -144,7 +144,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     ALLOWED_HOSTS = ['*']
 
     # Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.abspath(__fil__))
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
