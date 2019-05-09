d08d71ec4ee9d2f49c883095e8d3a9c560516d46
Karl Hiner <karl@cozy.co>
2019-05-08 19:24:12 -0700

    use `to_jshtml` for better animation quality

--- ipython_animation.py
+++ ipython_animation.py
@@ -5,8 +5,11 @@ from IPython.display import HTML
 MILLIS_PER_SECOND = 1_000
 DEFAULT_FPS = 24
 
-def create_animation(fig, plt, animate, length_seconds=4, frames_per_second=DEFAULT_FPS, frames=None):
+plt.rcParams["animation.writer"] = 'ffmpeg'
+plt.rcParams["animation.embed_limit"] = 100
+
+def create_animation(fig, plt, animate, length_seconds=4, frames_per_second=DEFAULT_FPS, frames=None, default_mode=None):
     anim = animation.FuncAnimation(fig, animate, frames=frames if frames else int(length_seconds * frames_per_second), interval=MILLIS_PER_SECOND/frames_per_second)
-    video = HTML(anim.to_html5_video())
+    video = HTML(anim.to_jshtml(default_mode=default_mode))
     plt.close()
     return video
