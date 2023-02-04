3a384115bc55bdd0e0b0f784c313d22caf09c987
Karl Hiner <karl.hiner@gmail.com>
2023-02-03 23:02:09 -0500

    Start Nonlinear Dynamics and Chaos

---
+++ nonlinear_dynamics_and_chaos/chapter_1.jl
@@ -0,0 +1,17 @@
+### A Pluto.jl notebook ###
+# v0.19.19
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 5b7718fa-a440-11ed-265a-17d3a16e7d91
+md"""
+# Nonlinear Dynamics and Chaos
+
+## Part 1: One-dimensional Flows
+
+## Chapter 1: Flows On the Line
+"""
+
+# ╔═╡ Cell order:
+# ╠═5b7718fa-a440-11ed-265a-17d3a16e7d91
